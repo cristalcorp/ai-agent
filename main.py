65b73487ba5c841380d14075f25389d95db4ed16
cristalcorp <cristal@cristalcorp.com>
2025-11-24 10:29:02 +0100

    c1l4

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
 
 
 def main():
-    print("Hello from ai-agent!")
+    #print("Hello from ai-agent!")
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    content = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    try:
+        _, arg = sys.argv
+    except ValueError:
+        print("Error: Missing 1 argument. You need to provide a prompt")
+        sys.exit(1)
+
+    content = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
     print(content.text)
     print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
