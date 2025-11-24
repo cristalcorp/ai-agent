d9a39c0ddf43cced3966d06f38f6ea061a35f6cf
cristalcorp <cristal@cristalcorp.com>
2025-11-24 11:33:07 +0100

    c1l5

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
-
+from google.genai import types
 
 
 def main():
@@ -13,15 +13,25 @@ def main():
     client = genai.Client(api_key=api_key)
 
     try:
-        _, arg = sys.argv
+        _, user_prompt = sys.argv
     except ValueError:
         print("Error: Missing 1 argument. You need to provide a prompt")
         sys.exit(1)
 
-    content = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
-    print(content.text)
-    print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+
+    #content = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
