1249c283f758839c1e2ab8b2f6d68bff1a7afa86
cristalcorp <cristal@cristalcorp.com>
2025-11-24 16:22:45 +0100

    c1l6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -12,14 +12,33 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    try:
-        _, user_prompt = sys.argv
-    except ValueError:
-        print("Error: Missing 1 argument. You need to provide a prompt")
-        sys.exit(1)
+    #try:
+    #    _, user_prompt = sys.argv
+    #except ValueError:
+    #    print("Error: Missing 1 argument. You need to provide a prompt")
+    #    sys.exit(1)
+
+    parser = argparse.ArgumentParser(
+        description="This is an ai-agent script, made using Boot.dev, thank you Boot.dev, you rock"
+    )
+
+    parser.add_argument(
+        "prompt",
+        type=str,
+        help="The prompt you should send to the llm"
+    )
+
+    parser.add_argument(
+        "-v", "--verbose",
+        action="store_true",
+        help="Display User Prompt, Prompt Tokens and Response Tokens"
+    )
+
+    args = parser.parse_args()
+
 
     messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=args.prompt)]),
     ]
 
     response = client.models.generate_content(
@@ -30,8 +49,10 @@ def main():
 
     #content = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
